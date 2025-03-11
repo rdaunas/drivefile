d53cbe0f37f564a06bc3e6db8d6e42f9cb8e98bc
rdaunas <romain.daunas@gmail.com>
2025-03-11 17:26:17 +0100

    fix schema type for singlestore

--- drizzle/0000_elite_the_captain.sql
+++ drizzle/0000_elite_the_captain.sql
@@ -1,9 +1,1 @@
-CREATE TABLE `file_table` (
-	`id` int AUTO_INCREMENT NOT NULL,
-	`name` text,
-	`type` text,
-	`url` text,
-	`parentId` int,
-	`size` text,
-	CONSTRAINT `file_table_id` PRIMARY KEY(`id`)
-);
+DROP TABLE `file_table`
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,10 +1,21 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import { int, text, singlestoreTable, index, bigint } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("file_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  type: text("type"),
-  url: text('url'),
-  parent: int("parentId"),
-  size: text("size")
+export const files = singlestoreTable("file_table", {
+  id: bigint("id", {mode: "number", unsigned: true}).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  url: text('url').notNull(),
+  parent: bigint("parent",{mode: "number", unsigned: true}).notNull(),
+  size: int("size").notNull()
+},
+(t) => {
+  return [index("parent_index").on(t.parent)]
+});
+
+export const folders = singlestoreTable("folder_table", {
+  id: bigint("id", {mode: "number", unsigned: true}).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent",{mode: "number", unsigned: true}),
+},
+(t) => {
+  return [index("parent_index").on(t.parent)]
 });
